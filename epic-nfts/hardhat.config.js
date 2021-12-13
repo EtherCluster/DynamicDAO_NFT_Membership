dba0220648af55d4d06fc84c02253fcf99cf86dd
EtherCluster <ethercluster@protonmail.com>
2021-12-13 17:37:33 +0200

    Test

--- epic-nfts/hardhat.config.js
+++ epic-nfts/hardhat.config.js
@@ -16,6 +16,14 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
-module.exports = {
-  solidity: "0.8.4",
-};
+ require('@nomiclabs/hardhat-waffle');
+
+ module.exports = {
+   solidity: '0.8.0',
+   networks: {
+     rinkeby: {
+       url: 'YOUR_ALCHEMY_API_URL',
+       accounts: ['YOUR_PRIVATE_RINKEBY_ACCOUNT_KEY'],
+     },
+   },
+ };
